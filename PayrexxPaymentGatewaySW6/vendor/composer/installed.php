55b512d3667e54c708e3cb8cde7de864d3169b7a
vinoth <vinoth@payrexx.com>
2025-08-07 14:48:11 +0530

    task/PP-15950: sdk updated version 2.0.2

--- PayrexxPaymentGatewaySW6/vendor/composer/installed.php
+++ PayrexxPaymentGatewaySW6/vendor/composer/installed.php
@@ -20,9 +20,9 @@
             'dev_requirement' => false,
         ),
         'payrexx/payrexx' => array(
-            'pretty_version' => 'v2.0.1',
-            'version' => '2.0.1.0',
-            'reference' => '09f68c0463b1c240f9b051caf0ec305e80f3f8e3',
+            'pretty_version' => 'v2.0.2',
+            'version' => '2.0.2.0',
+            'reference' => '88ebd2d02e1809fff7b5fa93b4a66e3ac0186e9b',
             'type' => 'library',
             'install_path' => __DIR__ . '/../payrexx/payrexx',
             'aliases' => array(),
