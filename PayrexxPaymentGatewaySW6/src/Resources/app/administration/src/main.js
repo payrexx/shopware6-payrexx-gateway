d62f9c357ffa33c1bd263a0937f86c302a413917
vinoth <vinoth@payrexx.com>
2025-08-14 10:27:41 +0530

    task/PP-16448: shows config link on settings and fixed connect button

--- PayrexxPaymentGatewaySW6/src/Resources/app/administration/src/main.js
+++ PayrexxPaymentGatewaySW6/src/Resources/app/administration/src/main.js
@@ -1,4 +1,4 @@
 import './module/payrexx-payment';
 import './api/payrexx-payment-settings.service';
-import './init/svg-icons.init';
+// import './init/svg-icons.init';
 import './module/payrexx-payment/components/connect-button/index';
