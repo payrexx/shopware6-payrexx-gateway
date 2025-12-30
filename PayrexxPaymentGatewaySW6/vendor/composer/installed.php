4d0fff657bba9f1469702abdac6d927dc74eed8e
vinoth <vinoth@payrexx.com>
2025-12-30 13:18:24 +0530

    bugfix/PP-17938: upgraded payrexx sdk for exclude the vat

--- PayrexxPaymentGatewaySW6/vendor/composer/installed.php
+++ PayrexxPaymentGatewaySW6/vendor/composer/installed.php
@@ -20,9 +20,9 @@
             'dev_requirement' => false,
         ),
         'payrexx/payrexx' => array(
-            'pretty_version' => 'v2.0.7',
-            'version' => '2.0.7.0',
-            'reference' => '2488323455e0508e99ebe58204d8112a8487efb8',
+            'pretty_version' => 'v2.0.8',
+            'version' => '2.0.8.0',
+            'reference' => '95473689ba082d327effbbc43fc2302d36c6c8bd',
             'type' => 'library',
             'install_path' => __DIR__ . '/../payrexx/payrexx',
             'aliases' => array(),
