57512daacf7a4442f9d3bb8a5381f46bef172d69
vinoth <vinoth@payrexx.com>
2026-01-09 15:40:13 +0530

    bugfix/PP-17938: optimized the autoloader

--- PayrexxPaymentGatewaySW6/vendor/composer/installed.php
+++ PayrexxPaymentGatewaySW6/vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'payrexx/payment',
-        'pretty_version' => '2.1.10',
-        'version' => '2.1.10.0',
+        'pretty_version' => '2.1.11',
+        'version' => '2.1.11.0',
         'reference' => null,
         'type' => 'shopware-platform-plugin',
         'install_path' => __DIR__ . '/../../',
@@ -11,8 +11,8 @@
     ),
     'versions' => array(
         'payrexx/payment' => array(
-            'pretty_version' => '2.1.10',
-            'version' => '2.1.10.0',
+            'pretty_version' => '2.1.11',
+            'version' => '2.1.11.0',
             'reference' => null,
             'type' => 'shopware-platform-plugin',
             'install_path' => __DIR__ . '/../../',
