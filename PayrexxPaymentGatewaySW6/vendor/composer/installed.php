70000f882ab03c5f5cbe054a33791b5809028f00
vinoth <vinoth@payrexx.com>
2026-03-12 16:13:10 +0530

    bugfix/PP-18521: updated the changes

--- PayrexxPaymentGatewaySW6/vendor/composer/installed.php
+++ PayrexxPaymentGatewaySW6/vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'payrexx/payment',
-        'pretty_version' => '2.1.12',
-        'version' => '2.1.12.0',
+        'pretty_version' => '2.1.13',
+        'version' => '2.1.13.0',
         'reference' => null,
         'type' => 'shopware-platform-plugin',
         'install_path' => __DIR__ . '/../../',
@@ -11,8 +11,8 @@
     ),
     'versions' => array(
         'payrexx/payment' => array(
-            'pretty_version' => '2.1.12',
-            'version' => '2.1.12.0',
+            'pretty_version' => '2.1.13',
+            'version' => '2.1.13.0',
             'reference' => null,
             'type' => 'shopware-platform-plugin',
             'install_path' => __DIR__ . '/../../',
@@ -22,7 +22,7 @@
         'payrexx/payrexx' => array(
             'pretty_version' => 'v2.0.12',
             'version' => '2.0.12.0',
-            'reference' => '3c65ea106c2b2ca444fd3c823fbc78be7cfeb75a',
+            'reference' => '6d249753a2b0aaf4b82ea73614c1ad0498d216be',
             'type' => 'library',
             'install_path' => __DIR__ . '/../payrexx/payrexx',
             'aliases' => array(),
